13c64feb28d083ec95454765ef360a0213154779
Muhammad Mudassir <muhammadmudassir33322@gmail.com>
2025-11-06 23:03:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,316 @@
+// start Here Js My Javascript journey Here
+
+// Let's start
+
+// Topic : 01
+// Variables
+
+// Var
+// Let
+// const
+// Name Types
+
+// snack case ( my_age )
+// cammel case( myName )
+// pascal case ( MyAge )
+
+// Let's Var Example
+
+var myName = "Mudassir";
+var my_name = "Ali";
+var Myname = "bilal";
+var MyName = "nehal";
+var myname = "shafa";
+var nameMy = "ubaid";
+var namemy = "waseem";
+var myname$ = "kamran";
+var NameMy$ = "kashif";
+var NameMy2 = "lareb";
+
+console.log(myName);
+console.log(my_name);
+console.log(Myname);
+console.log(MyName);
+console.log(myname);
+console.log(nameMy);
+console.log(namemy);
+console.log(myname$);
+console.log(NameMy$);
+console.log(NameMy2);
+
+// Kia ham var ko Redeclear kar sakta han ?
+
+// yes  ager class change kar kar kare ya nahi  kare pher bahi har kar skte han
+
+// Kia ham var ko Reassignment kar sakta han ?
+
+// Ans=>  yes kar sakte han
+
+// jase ka Foe example hamre pass aik variable ha
+
+var $MyName = "Mudassir";
+
+$MyName = "Muhammad Mudassir";
+
+console.log($MyName); // Answer Muhammad Mudassir
+
+//Let Examples Here
+
+let age = 0;
+let age1 = 1;
+let Age = 2;
+let MyAge = 3;
+let my_age = 4;
+let $myage = 5;
+let myage = 6;
+let myAge = 7;
+let my_age2 = 8;
+let Myage = 9;
+
+console.log(age);
+console.log(age1);
+console.log(Age);
+console.log(MyAge);
+console.log(my_age);
+console.log($myage);
+console.log(myage);
+console.log(myAge);
+console.log(my_age2);
+console.log(Myage);
+
+// Kia ham let ko Redeclear kar sakta han ?
+
+// No  ager class cahnge kar kar kare to hogye ga wase nahi hoga
+
+// Kia ham let ko Reassignment kar sakta han ?
+
+// Ans=>  yes kar sakte han
+
+// jase ka Foe example hamre pass aik variable ha
+
+let MyAge$ = 9;
+
+MyAge$ = 20;
+
+console.log(MyAge$); // Answer 20
+
+// Const Example Here
+
+const My_1class = "01";
+const My_class = "02";
+const Myclass = "03";
+const MyClass = "04";
+const Myclass1 = "05";
+const MyClass$ = "06";
+const myclass = "07";
+const myClass = "08";
+const myClass2 = "09";
+const Myclass3 = "10";
+
+console.log(My_1class);
+console.log(My_class);
+console.log(Myclass);
+console.log(MyClass);
+console.log(Myclass1);
+console.log(MyClass$);
+console.log(myclass);
+console.log(myClass);
+console.log(myClass2);
+console.log(Myclass3);
+
+// Kia ham cont ko Redeclear kar sakta han ?
+
+// No  ager class cahnge kar kar kare to bahi nahi hoga
+
+// Kia ham const ko Reassignment kar sakta han ?
+
+// Ans=>   Nahi kar sakte han
+
+// jase ka Foe example hamre pass aik variable ha
+
+const $Myclass = 9;
+
+MyAge$ = 20;
+
+console.log(MyAge$); // Answer 20
+
+// Let's start if else
+
+// if(condition) {
+// console.log("Ager condition True ho to Yeah chalye ")
+// }
+// else{
+// console.log("Ager condition false ho to yeah cahlye")
+// }
+
+// Possitive & Nagative Number Cheaker start Here just uncomment code
+
+// let GetNumber = prompt(" Enter the Positive & Nagative Number ");
+
+// if (GetNumber < 0) {
+//   alert(" This Number is Nagative");
+// } else {
+//   alert(" This Number is positive");
+// }
+
+// Possitive & Nagative Number Cheaker End Here
+
+// Even & odd Cheaker start Here just uncomment code
+
+// let GetNumbers = prompt(" Enter Even & Odd Numbers ");
+
+// if (GetNumbers % 2 == 0) {
+//   alert(`${GetNumbers} is Even Numbers`);
+// } else {
+//   alert(`${GetNumbers} is Odd Number`);
+// }
+
+// Even & odd Cheaker End Here
+
+//
+
+// if (condition3) {
+//   console.log("");
+// } else {
+//   console.log("");
+// }
+
+// if (condition4) {
+//   console.log("");
+// } else {
+//   console.log("");
+// }
+
+// if (condition5) {
+//   console.log("");
+// } else {
+//   console.log("");
+// }
+
+// if (condition6) {
+//   console.log("");
+// } else {
+//   console.log("");
+// }
+
+// if (condition7) {
+//   console.log("");
+// } else {
+//   console.log("");
+// }
+
+// if (condition8) {
+//   console.log("");
+// } else {
+//   console.log("");
+// }
+
+// if (condition9) {
+//   console.log("");
+// } else {
+//   console.log("");
+// }
+
+// if (condition10) {
+//   console.log("");
+// } else {
+//   console.log();
+// }
+
+// ise ham use karte han koi Condition da kar ceak karna jase ka ager ham na aik ifelse banya >
+// if(){
+//
+// }
+//
+//
+//
+
+// Ab ham if else ki aik type or deakhyn ga  jo ha else if ager hamre pass condition ziyada hoon to ham istarh ka sentaxt use karnga
+//
+// if(condition 1) {
+// alert ("")
+// }
+// else if( condition 2){
+// alert ("")
+// }
+// else{
+// console.log("")
+// }
+//
+
+// Else If Condition
+
+// Traffic controller RED YELLOW GREEN start Here
+
+// let signal = prompt("Enter Signal Color RED Yellow GREEN ");
+
+// if (signal == "red") {
+//   alert("Stop");
+// } else if (signal == "yellow") {
+//   alert("Let's Ready To Go");
+// } else if (signal == "green") {
+//   alert("Let's Go");
+// } else {
+//   alert("Enter Correct Signal Color And Speeling");
+// }
+
+// Traffic controller RED YELLOW GREEN End Here
+
+
+
+// Grade system Start Here
+
+// let marks = prompt("Enter Your Marks and Geeting  Grade");
+
+
+// if (marks >= 90 && marks <= 100 ) {
+//     alert("A+");
+// } else if (marks >= 80 && marks <= 89) {
+//     alert("A");
+// }  else if (marks <= 79) {
+//     alert("Fail")
+// } else {
+//     alert("Data is not avalible");
+// }
+ 
+// Grade system End Here
+
+
+
+
+
+
+    //Let's learn Nested if 
+    // else if or nested if in ma jo difrence ha wo yeah ha ka else if hota ha wo phle condition cheak kata ha ager wo false hotye ha to wo second else if cheak karta ha or  nested if ha wo phle condition true  honye ki surat ma dosre condition cheak kara ha wara dirtct else da data ha
+
+    
+
+
+    // let userAge = prompt("Enter Your Age ");
+    
+    // let userGender = prompt("Enter Your Gender Male & Female");
+    
+    // if ( userAge >= 18) {
+
+    //     let userGender = prompt("Enter Your Gender Male & Female");
+
+    //     if(userGender == "male"){
+    //         alert("Eligable");
+    //     }
+    //     else if(userGender == "female"){
+    //         alert("Eligable");
+            
+    //     }
+    //     else{
+    //         alert("Define Your Gender")
+    //     }
+    // } 
+    // else {
+    //         alert("Nots Eligable");
+        
+    // }
+
+
+
+
+
